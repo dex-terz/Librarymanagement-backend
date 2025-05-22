6cf24ff282e9bfd4dc9e774684971a1ceae937a0
santhoshi17-29 <santhoshi2908@gmail.com>
2025-05-22 13:46:55 +0530

    Initial commit for backendz

--- backend.js
+++ backend.js
@@ -9,13 +9,22 @@ const port = 3000;
 const PDFDocument = require('pdfkit');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false } 
 });
 
-
+module.exports = client;
+client.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Connection failed', err);
+  } else {
+    console.log('Connected successfully at', res.rows[0].now);
+  }
+});
 client.connect()
   .then(() => {
     console.log('Connected to PostgreSQL database successfully!');
